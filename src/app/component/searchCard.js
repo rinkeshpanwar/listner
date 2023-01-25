bcaf5e8a041a6573ba72a5a703b916ac93f8f60a
rinkesh panwar <62168730+rinkeshpanwar@users.noreply.github.com>
2023-01-25 15:59:48 +0000

    added popular question

--- src/app/component/searchCard.js
+++ src/app/component/searchCard.js
@@ -1,34 +1,61 @@
 import React from 'react'
-import {BiUpvote} from 'react-icons/bi';
+import {BiDownvote, BiUpvote} from 'react-icons/bi';
 
 function SearchCard() {
   return (
     <div className='w-full'>
-        <div className='flex '>
-            <p className='font-primary line-clamp-1 font-medium text-lg text-primary_black'>How to setup node js in windows 10?</p>
-            <div></div>
-            <div className='rounded-sm ml-2 text-primary_white font-primary px-2 text-lg  bg-green-600'>Accepted</div>
+        <div className='flex'>
+            <p className='font-primary line-clamp-1 font-medium text-lg text-primary_black cursor-pointer'>How to setup node js in windows 10?</p>
+            <div className='rounded-sm ml-2 text-primary_white font-primary px-2 text-lg cursor-pointer bg-green-600'>Accepted</div>
         </div>
-        <div className="font-secondary text-base line-clamp-3 text-slate-800 mt-2">
-            lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <div className="mt-5 font-secondary text-base text-slate-800 bg-stone-200 rounded-lg pl-3 py-4">
+            <div className='line-clamp-3'>
+                lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </div>
         </div>
         <div className='mt-3 flex items-center'>
             <div className='line-clamp-1 space-x-2'>
-                <div className='font-primary text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
                 </div>
-                <div className='font-primary text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
                 </div>
-                <div className='font-primary text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
                 </div>
-                <div className='font-primary text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
                 </div>
-                <div className='font-primary text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
+                </div>
+                <div className='font-primary cursor-pointer text-sm max-w-[100px] truncate inline-block px-2 py-1 bg-slate-200 rounded-sm text-indigo-600 '>Tafsfsfdsfsdfsdfsdfdsfsfsdfsdfs1
                 </div>
             </div>
-            <BiUpvote className='ml-3 text-xl text-green-400' />
-            <p className='font-secondary text-base text-green-600'>10 Upvote</p>
+            <div className="flex items-center min-w-max">
+                <BiUpvote className='ml-3 text-xl text-green-400' />
+                <p className='font-secondary text-base font-medium text-green-600'>10 Upvote</p>
+                <BiDownvote className='ml-3 text-xl text-red-400' />
+                <p className='font-secondary text-base text-red-600 font-medium'>10 Downvote</p>
+            </div>
         </div>
     </div>
   )
--- src/app/page/searchResult.js
+++ src/app/page/searchResult.js
@@ -17,9 +17,9 @@ function SearchResult() {
     // });
 
     return (
-        <div className='w-screen'>
+        <div className='w-full'>
             <div className='flex justify-between font-primary px-10 mt-10'>
-                <p className='line-clamp-1 font-medium text-xl'>Search Result &ldquo; {searchState.search} &rdquo;</p>
+                <p className='line-clamp-1 text-lg'>Search Result &ldquo; <span className='italic'>{searchState.search}</span> &rdquo;</p>
                 <button className='text-indigo-400 text-lg' onClick={() => setAdvaceSearchPopup((prev) => !prev)}>
                     Advance search 
                 </button>
@@ -29,11 +29,29 @@ function SearchResult() {
                     </div>
                 </Modal>
             </div>
-            <div className='flex px-10 mt-5'>
-                <div className='w-4/5'>
+            <div className='flex pr-10 lg:pr-0 pl-10 mt-5 gap-4 lg:gap-10 flex-wrap-reverse lg:flex-nowrap'>
+                <div className='lg:w-[83%] space-y-8'>
+                    <SearchCard />
+                    <SearchCard />
+                    <SearchCard />
+                    <SearchCard />
+                    <SearchCard />
                     <SearchCard />
                 </div>
-                <div className=''>other information</div>
+                <div className='w-full lg:w-[27%]'>
+                    <div className='font-primary mt-2 font-medium text-lg'>Popular questions</div>
+                    <div className='font-secondary lg:mt-3 space-y-2 font-light text-zinc-800'>
+                        <div className='line-clamp-3'>
+                           What is the best way to learn react?
+                        </div>
+                        <div className='line-clamp-3'>
+                           What is the best way to learn react?
+                        </div>
+                        <div className='line-clamp-3'>
+                           What is the best way to learn react?
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
